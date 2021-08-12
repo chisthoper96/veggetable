e01565ab682e8ea9c641c26acccab9bf20b97c4d
Chisthoper Mella <chisthoper96@gmail.com>
2021-08-11 20:54:53 -0400

    creacion de modelo ingredient, controller, view search.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_210319) do
+ActiveRecord::Schema.define(version: 2021_08_11_184336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name_ingredient"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name_recipes"
