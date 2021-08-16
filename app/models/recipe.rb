972a5278cb60da09dda050d48d3cbcf600882952
Chisthoper Mella <chisthoper96@gmail.com>
2021-08-16 19:44:44 -0400

    modelo recetasfavoritas y pagina show

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,3 @@
 class Recipe < ApplicationRecord
+  has_many :recipefavorites
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_195813) do
+ActiveRecord::Schema.define(version: 2021_08_16_202233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name_ingredient"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "recipefavorites", force: :cascade do |t|
+    t.bigint "recipes_id"
+    t.bigint "users_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["recipes_id"], name: "index_recipefavorites_on_recipes_id"
+    t.index ["users_id"], name: "index_recipefavorites_on_users_id"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -46,4 +76,7 @@ ActiveRecord::Schema.define(version: 2021_08_13_195813) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "recipefavorites", "recipes", column: "recipes_id"
+  add_foreign_key "recipefavorites", "users", column: "users_id"
 end
