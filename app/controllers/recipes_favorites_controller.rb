e566854483bb161417ec37057b5d64024a0275e1
Chisthoper Mella <chisthoper96@gmail.com>
2021-08-23 18:47:42 -0400

    boton favoritos listo

--- app/controllers/recipes_favorites_controller.rb
+++ app/controllers/recipes_favorites_controller.rb
@@ -6,25 +6,17 @@ class RecipesFavoritesController < ApplicationController
 
   def create
     @recipe = Recipe.find(params[:recipe_id])
-    if @recipe.user_id != current_user.id
-      @favorite = Recipefavorite.create(user_id: current_user.id, recipe_id: @recipe.id)
-     # @favorite.user = current_user.id
-    end
+    @favorite = Recipefavorite.new
+    @favorite.recipe = @recipe
+    @favorite.user = current_user
     @favorite.save
+    redirect_to recipe_path(@recipe)
   end
 
   def destroy
-    @recipe = Post.find(params[:post_id])
-    @favorite = favorite.find_by(user_id: current_user.id, recipe_id: @recipe.id)
-    @like.destroy
+    @recipe = Recipe.find(params[:recipe_id])
+    @favorite = Recipefavorite.find(params[:id])
+    @favorite.destroy
+    redirect_to @recipe
   end
-
-  def update
-  end
-
-  # private
-
-  # def favorite_params
-  #   params.require(:recipes_favorites).permit(:recipe_id)
-  # end
 end
